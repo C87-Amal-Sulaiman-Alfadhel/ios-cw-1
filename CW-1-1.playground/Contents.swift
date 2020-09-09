b31efe074fa2c3a7f6c7d66465d9fab4d8b03c1c
C87-Amal-Sulaiman-Alfadhel <70811155+C87-Amal-Sulaiman-Alfadhel@users.noreply.github.com>
2020-09-09 20:16:58 +0300

    classwork done

---
+++ CW-1-1.playground/Contents.swift
@@ -0,0 +1,19 @@
+
+let name = "Amal"
+var height = 165
+var age = 17
+var hobby1 = "Watching Formula 1"
+var hobby2 = "Running"
+print ("My name is \(name)\nmy age is: \(age)")
+
+var friendName = "Mariam"
+var friendHeight = 164
+var friendAge = 18
+var friendHobby1 = "Running"
+var friendHobby2 = "Shopping"
+
+var ageDifference = age - friendAge
+print (ageDifference)
+
+var mutualHobby = hobby1 == friendHobby1 || hobby1 == friendHobby2 || hobby2 == friendHobby1 || hobby2 == friendHobby2
+print (mutualHobby)
